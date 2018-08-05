5fec3c798457d460f2a649832763bbb7246f7702
v-docapo <v-docapo@microsoft.com>
2018-08-05 15:57:37 -0700

    Added option on home menu

--- BoardGameSuite/Program.cs
+++ BoardGameSuite/Program.cs
@@ -8,16 +8,17 @@ namespace BoardGameSuite
         {
             GameFactory gameFactory = new GameFactory();
             Console.WriteLine("Which game would you like to play? ");
-            Console.WriteLine("Wheel of Fortune (1)");
+            Console.WriteLine("1 ) Wheel of Fortune");
+            Console.WriteLine("2 ) Connect Four");
             bool validOption = false;
             int option = -1;
             while (!validOption)
             {
                 string optionInput = Console.ReadLine();
-                validOption = Int32.TryParse(optionInput, out option) && option > 0 && option < 2;
+                validOption = Int32.TryParse(optionInput, out option) && option > 0 && option < 3;
                 if (!validOption)
                 {
-                    Console.WriteLine("Enter a number ( 1 )");
+                    Console.WriteLine("Enter a number on the list");
                 }
             }
             Game game = gameFactory.CreateGame(option);
